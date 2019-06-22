377df216e55c6e04552d259b716ec3fb096e49bd
Anna Sanderson <anna.sanderson@csuglobal.edu>
2019-06-22 14:47:58 -0400

    added views

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-module.export = app;
+const appConfig = require("./config/main-config.js");
+const routeConfig = require("./config/route-config.js");
 
- const appConfig = require("./config/main-config.js");
- const routeConfig = require("./config/route-config.js");
+appConfig.init(app, express);
+routeConfig.init(app);
 
- appConfig.init();
- routeConfig.init(app);
+module.exports = app;
