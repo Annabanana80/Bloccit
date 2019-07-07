2c5d2680851f050cfb5521345ab2b4d8b0f087b7
Anna Sanderson <annasanderson@annas-air.home>
2019-07-06 22:39:34 -0400

    added about route, testing, and about on the menu

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,11 +8,40 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2
-   it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
-   request.get(base, (err, res, body) => {
-     expect(res.statusCode).toBe(200);
-     expect(body).toContain("Welcome to Bloccit");
+    it("should return status code 200", (done) => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
         done();
+      });
+    });
+
+  });
+
+  describe("GET /marco", () => {
+
+  it("should returns a status code 200", () => {
+    request.get(`${base}marco`, (err, res, body) => {
+      expect(res.statusCode).toBe(200);
+    });
+  });
+
+  it("should have 'Welcome to Bloccit' in the body of the response", () => {
+    request.get(`${base}marco`, (err, res, body) => {
+      expect(body).toBe("polo");
+    });
+  });
+
+});
+
+  describe("GET /about", () => {
+
+    it("should return 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+      request.get(`${base}about`, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
       });
     });
 
